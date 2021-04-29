f8c22fde0a3b724aef7fafd44fa7a1a76f7bf228
Mojtaba Aradmehr <m.aaradmehr@gmail.com>
2021-04-29 13:12:15 +0430

    init button-view branch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-sdf
+var box = document.querySelector('.showfront');
+// var arrowRight = document.getElementById("arrow-right");
+document.getElementById("arrow-right").addEventListener( 'click', goSide );
+document.getElementById("arrow-left").addEventListener( 'click', goSide );
+
+
+
+function goSide() {
+    var  currentClass = box.className;
+    switch (currentClass) {
+        case 'showfront':
+            box.classList.remove( currentClass );
+            box.classList.add( 'showright' );
+            break;
+        case 'showright':
+            box.classList.remove( currentClass );
+            box.classList.add( 'showback' );
+            break;   
+        case 'showback':
+            box.classList.remove( currentClass );
+            box.classList.add( 'showleft' );
+            break;
+        case 'showleft':
+            box.classList.remove( currentClass );
+            box.classList.add( 'showfront' );
+            break;                            
+    }
+
+  }
